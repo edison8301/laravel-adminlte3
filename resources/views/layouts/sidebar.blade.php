df89e08ef846f848aee112bd5a17e57e76b5c038
Thomas Alfa Edison <thomas.alfa.edison@gmail.com>
2020-06-16 09:30:16 +0700

    Gambar sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -14,7 +14,7 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="{{ asset('/images/avatar.jpeg') }}" class="img-circle elevation-2" alt="User Image">
+                <img src="{{ asset('/images/user.jpeg') }}" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
                 <a class="d-block" href="#">{{ Auth::user()->username }}</a>
