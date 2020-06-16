aa3a47504a47cc856db10366c519c25092a48a2e
Thomas Alfa Edison <thomas.alfa.edison@gmail.com>
2020-06-16 10:27:15 +0700

    'update'

--- resources/views/layouts/backend/sidebar.blade.php
+++ resources/views/layouts/backend/sidebar.blade.php
@@ -6,7 +6,7 @@
     <a href="index3.html" class="brand-link">
         <img src="{{ asset('/images/logo-sidebar.png') }}" alt="TOR Bappenas" class="brand-image img-circle elevation-3"
              style="opacity: .8">
-        <span class="brand-text font-weight-light">TOR BAPPENAS</span>
+        <span class="brand-text font-weight-light">Nama Aplikasi</span>
     </a>
 
     <!-- Sidebar -->
